0dc98a62fba6d8daee0f52c71199ac525aaeacfb
Tyas Lambrechts <tyas.lambrechts@external.atlascopco.com>
2023-05-30 23:42:48 +0200

    tomorrow the other stuff

--- index.js
+++ index.js
@@ -13,15 +13,45 @@ const PRIVATE_APP_ACCESS = '';
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const customObject = 'https://api.hubapi.com/crm/v3/objects/custom-objects/properties/names?includeProperties=true';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(customObject, { headers });
+        const data = resp.data.results;
+        res.render('index', { title: 'Custom Objects | HubSpot APIs', data });      
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get('/update-cobj', async (req, res) => {
+    const customObject = 'https://api.hubapi.com/crm/v3/objects/custom-objects/properties/names?includeProperties=true';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(customObject, { headers });
+        const data = resp.data.results;
+        res.render('update-cobj', { title: 'Update Custom Objects | HubSpot APIs', data });      
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
-
+app.post('/update-cobj', async (req, res) => {
+    
+});
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
 
